efc697ddd35cf41cf6dc3da8f501367c8bb2e0f3
Zander Ambrose <alexander.d.ambrose@gmail.com>
2021-06-29 17:08:41 -0400

    Finished computing logic for all four operators.  Added error checking for the negative and decimal buttons

--- oop.js
+++ oop.js
@@ -2,41 +2,72 @@
 const numberButtons = document.querySelectorAll('[data-typeof="number"]')
 const operatorButtons = document.querySelectorAll('[data-typeof="operator"]')
 const decimal = document.querySelector('[data-typeof="decimal"]')
+const negativeButton = document.querySelector('.negative')
+const negativeText = document.querySelector('[data-typeof="negative"]')
 const equalButton = document.querySelector('[data-typeof="equal"]')
 const clearButton = document.querySelector('[data-typeof="clear"]')
-const currentDisplayComputation = document.querySelector('.displayNum')
-const previousDisplayComputation = document.querySelector('.computationDisplay')
+const currentDisplay = document.querySelector('.displayNum')
+const previousDisplay = document.querySelector('.computationDisplay')
 
 // Calculator CLASS
 class Calculator {
-    constructor(currentDisplayComputation, previousDisplayComputation) {
-        this.currentDisplayComputation = currentDisplayComputation
-        this.previousDisplayComputation = previousDisplayComputation
+    constructor(currentDisplay, previousDisplay) {
+        this.currentDisplay = currentDisplay
+        this.previousDisplay = previousDisplay
         this.clear()
     }
 
     clear() {
         this.currentOperatorValue = null
-        this.currentComputationValue = 0
+        this.currentComputationValue = ''
         this.previousComputationValue = ''
         this.updateDisplay()
     }
 
     updateValue(number) {
-        this.currentComputationValue = this.previousComputationValue + number
-        this.previousComputationValue = this.currentComputationValue
+        this.currentComputationValue = this.currentComputationValue + number
     }
+
     updateDisplay() {
-        this.currentDisplayComputation.innerText = this.currentComputationValue
+        if (this.currentComputationValue === '') {
+            this.currentDisplay.textContent = '0'
+        } else {
+            this.currentDisplay.textContent = this.currentComputationValue
+        }
+    }
+
+    updatePreviousDisplay(operator) {
+        this.currentOperatorValue = operator
+        this.previousComputationValue = this.currentComputationValue
+        this.previousDisplay.textContent = `${this.currentDisplay.textContent} ${this.currentOperatorValue}`
+        this.currentComputationValue = ''
+        this.updateDisplay()
     }
 
     calculate() {
-        console.log('calculated')
+        let currentComputationValuePlaceholder = this.currentComputationValue
+        if (this.currentOperatorValue == '+') {
+            this.currentComputationValue = ''
+            this.updateValue((parseFloat(this.previousComputationValue) + parseFloat(currentComputationValuePlaceholder)).toString())
+            this.updateDisplay()
+        } else if (this.currentOperatorValue == '-') {
+            this.currentComputationValue = ''
+            this.updateValue((parseFloat(this.previousComputationValue) - parseFloat(currentComputationValuePlaceholder)).toString())
+            this.updateDisplay()
+        } else if (this.currentOperatorValue == '*') {
+            this.currentComputationValue = ''
+            this.updateValue((parseFloat(this.previousComputationValue) * parseFloat(currentComputationValuePlaceholder)).toString())
+            this.updateDisplay()
+        } else if (this.currentOperatorValue == '/') {
+            this.currentComputationValue = ''
+            this.updateValue((parseFloat(this.previousComputationValue) / parseFloat(currentComputationValuePlaceholder)).toString())
+            this.updateDisplay()
+        }
     }
 }
 
 // Creating a new object calc with Calculator class
-const calc = new Calculator(currentDisplayComputation, previousDisplayComputation)
+const calc = new Calculator(currentDisplay, previousDisplay)
 
 // Add event listeners for the NUMBER BUTTONS
 numberButtons.forEach(button => {
@@ -46,8 +77,34 @@ numberButtons.forEach(button => {
     })
 })
 
+// Add event listener for NEGATIVE BUTTON
+negativeButton.addEventListener('click', () => {
+    if (currentDisplay.textContent.includes('-')) return
+    calc.updateValue(negativeText.textContent)
+    calc.updateDisplay()
+})
+
+// Add event listener for DECIMAL BUTTON
+decimal.addEventListener('click', () => {
+    if (currentDisplay.textContent.includes('.')) return
+    calc.updateValue(decimal.textContent)
+    calc.updateDisplay()
+})
+
 // Add event listener for the CLEAR BUTTON
 clearButton.addEventListener('click', () => {
     calc.clear()
+})
+
+// Add event listeners for the OPERATOR BUTTONS
+operatorButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calc.updatePreviousDisplay(button.textContent)
+    })
+})
+
+// Add event listener for EQUAL BUTTON  
+equalButton.addEventListener('click', () => {
+    calc.calculate()
 })
 
